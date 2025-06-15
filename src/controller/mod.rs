6954e587158bfe4a9882dd7364cbd87090de4ea6
jobin_nelson <jobinnelson369@gmail.com>
2025-06-15 11:07:50 +0530

    feat: list task

--- src/controller/mod.rs
+++ src/controller/mod.rs
@@ -1,11 +1,14 @@
-use crate::{cli::ListArg, Cli, Result};
-use crate::cli::{AddArg, Op};
+use crate::{
+    cli::{AddArg, ListArg, Op},
+    objects::Task,
+    Cli, Result,
+};
 use sqlx::SqlitePool;
 
 pub async fn delegate(db: &SqlitePool, cli: Cli) -> Result<()> {
     let op = cli.op.unwrap_or_else(|| Op::List(ListArg::default()));
     match op {
-        Op::List(list_arg) => todo!(),
+        Op::List(list_arg) => list_task(db, list_arg, &mut std::io::stdout()).await?,
         Op::Add(add_arg) => add_task(db, add_arg).await?,
         Op::Done => todo!(),
         Op::Edit => todo!(),
@@ -24,6 +27,20 @@ pub async fn add_task(db: &SqlitePool, add_arg: AddArg) -> Result<()> {
     .bind(add_arg.description)
     .fetch_one(db)
     .await?;
+
+    Ok(())
+}
+
+pub async fn list_task<T: std::io::Write>(
+    db: &SqlitePool,
+    add_arg: ListArg,
+    mut writer: T,
+) -> Result<()> {
+    let tasks: Vec<Task> = sqlx::query_as("SELECT * FROM tasks").fetch_all(db).await?;
+
+    for task in tasks {
+        writeln!(writer, "- {}", task.title)?;
+    }
 
     Ok(())
 }
--- tests/test_task.rs
+++ tests/test_task.rs
@@ -1,5 +1,9 @@
 use clap::Parser;
-use twodo::{controller::delegate, objects::Task, Cli};
+use twodo::{
+    controller::{delegate, list_task},
+    objects::Task,
+    Cli,
+};
 
 type Result<T> = core::result::Result<T, Box<dyn std::error::Error>>; // For tests.
 
@@ -23,5 +27,30 @@ async fn test_add_tasks() -> Result<()> {
 
     let result_title: String = task.title;
     assert_eq!(result_title, task_title);
+    Ok(())
+}
+
+#[tokio::test]
+async fn test_list_tasks() -> Result<()> {
+    // -- Setup & Fixtures
+    let db = sqlx::sqlite::SqlitePool::connect("sqlite::memory:").await?;
+    // create table
+    sqlx::migrate!("./migrations").run(&db).await?;
+
+    // -- Exec
+    let task_title = "'Read Harry Potter'";
+    let args = Cli::try_parse_from(["twodo", "add", task_title])?;
+    delegate(&db, args).await?;
+
+    // -- Check
+    let mut stdout = Vec::new();
+    let args = Cli::try_parse_from(["twodo", "list"])?;
+    match args.op {
+        Some(twodo::cli::Op::List(list_arg)) => list_task(&db, list_arg, &mut stdout).await?,
+        _ => panic!("Expected list operation"),
+    };
+    assert!(stdout
+        .windows(task_title.len())
+        .any(move |sub_slice| sub_slice == task_title.as_bytes()));
     Ok(())
 }
